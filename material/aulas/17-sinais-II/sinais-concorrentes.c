04f122baedf1fdbf711b5e0b60394beec7aeac3c
leonardodma <leonardodma@al.insper.edu.br>
2021-11-11 16:32:12 -0300

    atualização do repo

--- material/aulas/17-sinais-II/sinais-concorrentes.c
+++ material/aulas/17-sinais-II/sinais-concorrentes.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 
 int status = 0;
 
-void operacao_lenta() {
+void operacao_lenta()
+{
     sleep(10);
 }
 
-
-void sigint_handler(int num) {
+void sigint_handler(int num)
+{
     status += 1;
     printf("Chamou Ctrl+C; status=%d\n", status);
     operacao_lenta();
     printf("SIGINT: Vou usar status agora! status=%d\n", status);
 }
 
-void sigterm_handler(int num) {
+void sigterm_handler(int num)
+{
     status += 1;
     printf("Recebi SIGTERM; status=%d\n", status);
     operacao_lenta();
     printf("SIGTERM: Vou usar status agora! status=%d\n", status);
 }
 
-int main() {
+int main()
+{
     /* TODO: registar SIGINT aqui. */
+    struct sigaction sigint;
+    sigint.sa_handler = sigint_handler; // aqui vai a função a ser executada
+    sigemptyset(&sigint.sa_mask);
+    sigint.sa_flags = 0;
+    sigaction(SIGINT, &sigint, NULL);
 
     /* TODO: registar SIGTERM aqui. */
+    struct sigaction sigterm;
+    sigterm.sa_handler = sigterm_handler; // aqui vai a função a ser executada
+    sigemptyset(&sigterm.sa_mask);
+    sigterm.sa_flags = 0;
+    sigaction(SIGTERM, &sigterm, NULL);
 
     printf("Meu pid: %d\n", getpid());
 
-    while(1) {
+    while (1)
+    {
         sleep(1);
     }
     return 0;
--- material/aulas/17-sinais-II/sinal1.c
+++ material/aulas/17-sinais-II/sinal1.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
 
-void sig_handler(int num) {
+int count_ctrl_c = 0;
+
+void sig_handler(int num)
+{
+    count_ctrl_c++;
+
+    if (count_ctrl_c >= 3)
+    {
+        struct sigaction s;
+        s.sa_handler = SIG_DFL; // aqui vai a função a ser executada
+        sigemptyset(&s.sa_mask);
+        s.sa_flags = 0;
+        sigaction(SIGINT, &s, NULL);
+    }
+
     printf("Chamou Ctrl+C\n");
 }
 
-int main() {
-    /* TODO: registre a função sig_handler
-     * como handler do sinal SIGINT
-     */
+int main()
+{
+    /* O exemplo abaixo cria um struct sigaction e o seta para 
+    executar um handler quando o processo receber SIGINT (Ctrl+C).
+    */
+    struct sigaction s;
+    s.sa_handler = sig_handler; // aqui vai a função a ser executada
+    sigemptyset(&s.sa_mask);
+    s.sa_flags = 0;
+    sigaction(SIGINT, &s, NULL);
+
     printf("Meu pid: %d\n", getpid());
 
-    while(1) {
+    while (1)
+    {
         sleep(1);
     }
     return 0;
