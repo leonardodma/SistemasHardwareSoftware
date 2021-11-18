4d764ffc08ecc834d24a055117ef4e8807b2288b
leonardodma <leonardodma@al.insper.edu.br>
2021-11-18 16:30:07 -0300

    atualizando repo

--- material/aulas/18-threads-I/exemplo1.c
+++ material/aulas/18-threads-I/exemplo1.c
@@ -2,20 +2,25 @@
 #include <pthread.h>
 #include <stdio.h>
 
-void *minha_thread(void *arg) {
+void *minha_thread(void *arg)
+{
     printf("Hello thread!\n");
     return NULL;
 }
 
-
-int main() {
+int main()
+{
     pthread_t tid;
-    
-    int error = pthread_create(&tid, NULL, minha_thread, NULL);
+
+    int error = pthread_create(
+        &tid,         // variável para guardar ID da nova thread
+        NULL,         // opções de criação. NULL = opções padrão
+        minha_thread, // função a ser executada
+        NULL);        // parâmetro passado para a função acima
 
     printf("Hello main\n");
-    
+
     pthread_join(tid, NULL);
-    
+
     return 0;
 }
